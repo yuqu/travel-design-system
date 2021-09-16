5629f0a495d5686000a3697d74a9b9de4ceb864e
Sertaç Karahoda <skarahoda@sabanciuniv.edu>
2021-09-16 16:07:35 +0300

    Initial setup

---
+++ src/Button.stories.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+
+import { Button } from './Button';
+
+export default {
+  title: 'Components / Button',
+  component: Button
+} as ComponentMeta<typeof Button>;
+
+const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />;
+
+export const Primary = Template.bind({});
+Primary.args = {
+  primary: true,
+  children: 'Button',
+};
+
+export const Secondary = Template.bind({});
+Secondary.args = {
+  children: 'Button',
+};
+
+export const Large = Template.bind({});
+Large.args = {
+  size: 'large',
+  children: 'Button',
+};
+
+export const Small = Template.bind({});
+Small.args = {
+  size: 'small',
+  children: 'Button',
+};
---
+++ src/Button.tsx
@@ -0,0 +1,7 @@
+import React, { FunctionComponent } from "react";
+
+export const Button: FunctionComponent = ({ children }) => (
+    <button>
+        {children}
+    </button>
+);
